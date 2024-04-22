b95c6833e7e8189be7c447f1f431cd1294d55828
syji35 <45880189+syji35@users.noreply.github.com>
2024-04-22 09:53:09 +0000

    Ajout Readme du back

--- back/README.md
+++ back/README.md
@@ -1,7 +1,36 @@
 # Backend de MegActe
 
+## Pre-requis
+
+* Installer Python3.11+
+* Installer Pip
+
 ## Installation
+
+Dans le dossier "back".
+* Créer un venv
+```
+python3 -m venv .venv
+```
+* Activer le venv
+```
+source .venv/bin/activate
+```
+
+* installer les dépendances
+```
+pip install -r requirement.txt
+```
 
 ## Configuration
 
-## API
+* Dans le dossier config, dupliquer le fichier "config_template.yml" en "config.yml"
+* Editer le fichier avec les bonnes valeurs
+
+## Lancement du back
+
+Dans le dossier back.
+``` 
+python3 manage.py
+```
+
--- back/manage.py
+++ back/manage.py
@@ -1,5 +1,15 @@
-from app.endpoints import app as application
+from app import create_app
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run the Flask application.')
+    parser.add_argument('--port', type=int, default=5000, help='Port number for the Flask application')
+    parser.add_argument('--host', type=str, default='127.0.0.1')
+    return parser.parse_args()
+
+app_flask = create_app()
 
 
 if __name__ == "__main__":
-    application.run()
+    args = parse_arguments()
+    app_flask.run(port=args.port,host=args.host)
