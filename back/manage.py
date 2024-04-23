d25cb996102a9729b4eee8fd87bb2d9b95b0d40e
syji35 <45880189+syji35@users.noreply.github.com>
2024-04-23 12:05:16 +0200

    test codacy

--- back/manage.py
+++ back/manage.py
@@ -1,15 +1,20 @@
 from app import create_app
 import argparse
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Run the Flask application.')
-    parser.add_argument('--port', type=int, default=5000, help='Port number for the Flask application')
-    parser.add_argument('--host', type=str, default='127.0.0.1')
+    parser = argparse.ArgumentParser(description="Run the Flask application.")
+    parser.add_argument(
+        "--port", type=int, default=5000, help="Port number for the Flask application"
+    )
+    parser.add_argument("--host", type=str, default="127.0.0.1")
     return parser.parse_args()
 
-app_flask = create_app()
 
+app_flask = create_app()
+if app_flask == False:
+    print("ko")
 
 if __name__ == "__main__":
     args = parse_arguments()
-    app_flask.run(port=args.port,host=args.host)
+    app_flask.run(port=args.port, host=args.host)
