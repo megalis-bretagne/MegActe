bf94834ef12b58ad2bdb8f4dd35a2d690ae8e342
syji35 <45880189+syji35@users.noreply.github.com>
2024-04-24 09:07:21 +0000

    maj linting

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialExampleModule} from '../material.module';
+import { MaterialExampleModule } from '../material.module';
 import { DeliberationsFormComponent } from './components/deliberations-form.component';
 import { NoTdtFormComponent } from './components/no-tdt-form.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DocumentsListComponent } from './documents-list/documents-list.component';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { SettingsHttpService } from 'src/environments/settings.http.service';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent, SafeHtmlPipe } from './components/dashboard.component';
 import { DeliberationsComponent } from './components/deliberations.component';
-import { NavbarComponent, DialogNavBar } from './components/navbar.component';
+import { NavbarComponent, DialogNavBarComponent } from './components/navbar.component';
 import { ActesAutresComponent } from './components/actes-autres.component';
 import { PastellSnackComponent } from './components/pastell-snack.component';
 import { SettingsService } from 'src/environments/settings.service';
@@ -30,7 +30,7 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
     DashboardComponent,
     SafeHtmlPipe,
     NavbarComponent,
-    DialogNavBar,
+    DialogNavBarComponent,
     PastellSnackComponent,
     ActesAutresComponent
   ],
@@ -43,7 +43,7 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
     ReactiveFormsModule,
     KeycloakAngularModule,
     HttpClientModule,
-    LoggerModule.forRoot({ level: NgxLoggerLevel.WARN }),    
+    LoggerModule.forRoot({ level: NgxLoggerLevel.WARN }),
   ],
   providers: [
     {
@@ -57,6 +57,6 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 })
 export class AppModule { }
 
-export function app_Init(settingsHttpService: SettingsHttpService): () => Promise<any>{
+export function app_Init(settingsHttpService: SettingsHttpService): () => Promise<any> {
   return () => settingsHttpService.initializeApp();
 }
