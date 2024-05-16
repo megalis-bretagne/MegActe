40bc95834ef184e4782aa6002b7584903e639f7f
Sylvain Jolivet <45880189+syji35@users.noreply.github.com>
2024-05-16 16:17:35 +0200

    Ajout de la fonction de validation et récupértion de l'utilisateur (#44)

--- back/app/routers/users.py
+++ back/app/routers/users.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from ..dependencies import get_current_user
 
 router = APIRouter()
 
 
-@router.get("/users", tags=["users"])
-def get_users():
-    return [{"username": "sylvain"}, {"username": "Jolivet"}]
+@router.get("/user/me", tags=["users"])
+def get_users(current_user: dict = Depends(get_current_user)):
+    return current_user
+
+
+@router.get("/users/{user_id}", tags=["users"])
+def get_user_by_id(user_id: int):
+    return user_id
