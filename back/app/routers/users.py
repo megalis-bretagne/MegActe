7849021c184835fd1c9b6e9a20c2090e5cd77026
BoussairiIkram <98105656+BoussairiIkram@users.noreply.github.com>
2024-05-24 09:52:14 +0000

    Séparation des préoccupations

--- back/app/routers/users.py
+++ back/app/routers/users.py
@@ -1,22 +1,16 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
-from ..models.users import UserPastell
 from ..database import get_db
 from ..dependencies import get_current_user
 from ..schemas.user_schemas import UserCreate
 from ..services.user_service import (
-    encrypt_password,
-    generate_key,
-    decrypt_password,
-    send_password_to_pastell,
+    get_all_users_from_db,
+    get_user_by_id_from_db,
+    add_user_to_db,
+    get_decrypted_password_from_db,
+    update_user_in_db,
+    delete_user_from_db,
 )
-import base64
-from ..exceptions.custom_exceptions import (
-    UserNotFoundException,
-    DecryptionException,
-)
-
 
 router = APIRouter()
 
@@ -27,9 +21,7 @@ router = APIRouter()
     tags=["users"],
     description="Récupère les informations de l'utilisateur connecté",
 )
-def get_user(
-    current_user: dict = Depends(get_current_user), db: Session = Depends(get_db)
-):
+def get_user(current_user: dict = Depends(get_current_user)):
     return current_user["login"]
 
 
@@ -38,89 +30,34 @@ def get_user(
 
 @router.get("/users/getAll", tags=["users"])
 def get_all_users(db: Session = Depends(get_db)):
-    users = db.query(UserPastell).all()
-    return users
+    return get_all_users_from_db(db)
 
 
 # Get user by id
 @router.get("/users/{user_id}", tags=["users"])
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
-    if db_user is None:
-        raise UserNotFoundException()
-    return db_user
+    return get_user_by_id_from_db(user_id, db)
 
 
 # Add user
 @router.post("/users/add", response_model=UserCreate, tags=["users"])
 def add_user(user_data: UserCreate, db: Session = Depends(get_db)):
-    """Ajouter un nouvel utilisateur dans la BD et envoyer le mot de passe non chifré à PASTELL
-
-    Args:
-        user_data (UserCreate): Les données du user à ajouter.
-        db (Session, optional): La session de base de données. Defaults to Depends(get_db).
-
-    Returns:
-        UserPastell: L'utilisateur nouvellement créé.
-    """
-    # Chiffrer le pwd
-    key = generate_key(user_data.pwd_pastell)
-    encrypted_pwd = encrypt_password(user_data.pwd_pastell, key)
-
-    new_user = UserPastell(
-        login=user_data.login,
-        id_pastell=user_data.id_pastell,
-        pwd_pastell=encrypted_pwd,
-        pwd_key=base64.urlsafe_b64encode(key).decode("utf-8"),
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    # Envoyer le pwd non chifré à PASTELL
-
-    send_password_to_pastell(user_data.id_pastell, user_data.pwd_pastell)
-
-    return new_user
+    return add_user_to_db(user_data, db)
 
 
 # Récupérer un pwd chiffré et le déchiffrer
 @router.get("/users/decrypt_password/{user_id}", tags=["users"])
 def get_decrypted_password(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
-    if not user:
-        raise UserNotFoundException()
-    key = base64.urlsafe_b64decode(user.pwd_key.encode("utf-8"))
-    try:
-        decrypted_password = decrypt_password(user.pwd_pastell, key)
-        return {"decrypted_password": decrypted_password}
-    except Exception:
-        raise DecryptionException()
+    return get_decrypted_password_from_db(user_id, db)
 
 
 # Update User
 @router.put("/users/{user_id}", response_model=UserCreate, tags=["users"])
 def update_user(user_id: int, user_data: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
-    if not db_user:
-        raise UserNotFoundException()
-
-    db_user.login = user_data.login
-    db_user.id_pastell = user_data.id_pastell
-    db.commit()
-    db.refresh(db_user)
-
-    return db_user
+    return update_user_in_db(user_id, user_data, db)
 
 
 # Delete User
 @router.delete("/users/{user_id}", tags=["users"])
 def delete_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
-    if not db_user:
-        raise UserNotFoundException()
-
-    db.delete(db_user)
-    db.commit()
-
-    return {"message": "User deleted successfully"}
+    return delete_user_from_db(user_id, db)
--- back/app/services/user_service.py
+++ back/app/services/user_service.py
@@ -5,6 +5,15 @@ import base64, os
 import requests
 from app.configuration import read_config
 from ..exceptions.custom_exceptions import PastellException
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
+
+from ..schemas.user_schemas import UserCreate
+from ..exceptions.custom_exceptions import (
+    UserNotFoundException,
+    DecryptionException,
+)
+from ..models.users import UserPastell
 
 
 def generate_key(password: str) -> bytes:
@@ -43,3 +52,88 @@ def send_password_to_pastell(id_pastell: int, password: str):
             status_code=response.status_code,
             detail="Failed to update password in Pastell",
         )
+
+
+# Get liste de tous les users
+def get_all_users_from_db(db: Session):
+    users = db.query(UserPastell).all()
+    return users
+
+
+# Get user by id
+def get_user_by_id_from_db(user_id: int, db: Session):
+    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
+    if db_user is None:
+        raise UserNotFoundException()
+    return db_user
+
+
+# Add user
+def add_user_to_db(user_data: UserCreate, db: Session):
+    """Ajouter un nouvel utilisateur dans la BD et envoyer le mot de passe non chifré à PASTELL
+
+    Args:
+        user_data (UserCreate): Les données du user à ajouter.
+        db (Session, optional): La session de base de données. Defaults to Depends(get_db).
+
+    Returns:
+        UserPastell: L'utilisateur nouvellement créé.
+    """
+    # Chiffrer le pwd
+    key = generate_key(user_data.pwd_pastell)
+    encrypted_pwd = encrypt_password(user_data.pwd_pastell, key)
+
+    new_user = UserPastell(
+        login=user_data.login,
+        id_pastell=user_data.id_pastell,
+        pwd_pastell=encrypted_pwd,
+        pwd_key=base64.urlsafe_b64encode(key).decode("utf-8"),
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    # Envoyer le pwd non chifré à PASTELL
+
+    send_password_to_pastell(user_data.id_pastell, user_data.pwd_pastell)
+
+    return new_user
+
+
+# Récupérer un pwd chiffré et le déchiffrer
+def get_decrypted_password_from_db(user_id: int, db: Session):
+    user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
+    if not user:
+        raise UserNotFoundException()
+    key = base64.urlsafe_b64decode(user.pwd_key.encode("utf-8"))
+    try:
+        decrypted_password = decrypt_password(user.pwd_pastell, key)
+        return {"decrypted_password": decrypted_password}
+    except Exception:
+        raise DecryptionException()
+
+
+# Update User
+def update_user_in_db(user_id: int, user_data: UserCreate, db: Session):
+    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
+    if not db_user:
+        raise UserNotFoundException()
+
+    db_user.login = user_data.login
+    db_user.id_pastell = user_data.id_pastell
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+# Delete User
+def delete_user_from_db(user_id: int, db: Session):
+    db_user = db.query(UserPastell).filter(UserPastell.id == user_id).first()
+    if not db_user:
+        raise UserNotFoundException()
+
+    db.delete(db_user)
+    db.commit()
+
+    return {"message": "User deleted successfully"}
