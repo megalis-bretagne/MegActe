13235e3e6f258aa3a3349c00bf961f9cc76d1b08
BoussairiIkram <98105656+BoussairiIkram@users.noreply.github.com>
2024-06-03 11:55:05 +0000

    Correction Linting code

--- web-app/src/app/services/userServices/user.service.ts
+++ web-app/src/app/services/userServices/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
 import { Observable } from 'rxjs';
