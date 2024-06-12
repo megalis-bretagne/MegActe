07613e0fd61f68848fd7da4225d629110599dd9e
BoussairiIkram <98105656+BoussairiIkram@users.noreply.github.com>
2024-06-12 14:08:25 +0000

    Solve : Deprecated Resolvers

--- web-app/src/app/resolvers/user.resolver.ts
+++ web-app/src/app/resolvers/user.resolver.ts
@@ -1,28 +1,24 @@
-import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/userServices/user.service';
 import { UserContext } from 'src/app/model/user.model';
 import { Acte } from 'src/app/model/acte.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserContextResolver implements Resolve<UserContext> {
-  constructor(private userService: UserService) { }
+export const UserContextResolver: ResolveFn<UserContext> = (
+  route,
+  state
+): Observable<UserContext> => {
+  const userService = inject(UserService);
+  return userService.getUserContext();
+};
 
-  resolve(): Observable<UserContext> {
-    return this.userService.getUserContext();
-  }
-}
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserFluxResolver implements Resolve<Acte[]> {
-  constructor(private userService: UserService) { }
+export const UserFluxResolver: ResolveFn<Acte[]> = (
+  route,
+  state
+): Observable<Acte[]> => {
+  const userService = inject(UserService);
+  return userService.getUserFlux();
+};
 
-  resolve(): Observable<Acte[]> {
-    return this.userService.getUserFlux();
-  }
-}
