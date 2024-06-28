d5afc144f9089ed7eccbe5f567eb8817b595bf2b
BoussairiIkram <98105656+BoussairiIkram@users.noreply.github.com>
2024-06-28 09:09:07 +0000

    Correction PR : Back

--- back/app/schemas/document_schemas.py
+++ back/app/schemas/document_schemas.py
@@ -5,26 +5,67 @@ from typing import List
 
 
 class DocUpdateInfo(BaseModel):
+    """
+    Modèle pour la mise à jour d'un document.
+
+    Attributes:
+        entite_id (int): L'ID de l'entité à laquelle le document est associé.
+        doc_info (Dict[str, Any]): Les informations mises à jour du document.
+    """
+
     entite_id: int
     doc_info: Dict[str, Any]
 
 
 class DocCreateInfo(BaseModel):
+    """
+    Modèle pour la création d'un nouveau document.
+
+    Attributes:
+        entite_id (int): L'ID de l'entité à laquelle le document sera associé.
+        flux_type (str): Le type de flux du document.
+        doc_info (Dict[str, Any]): Les informations initiales du document.
+    """
+
     entite_id: int
     flux_type: str
     doc_info: Dict[str, Any]
 
 
 class DeleteFileFromDoc(BaseModel):
+    """
+    Modèle pour la suppression d'un fichier d'un document.
+
+    Attributes:
+        entite_id (int): L'ID de l'entité à laquelle le document est associé.
+        file_name (str): Le nom du fichier à supprimer.
+    """
+
     entite_id: int
     file_name: str
 
 
 class AddFileToDoc(BaseModel):
+    """
+    Modèle pour l'ajout d'un fichier à un document.
+
+    Attributes:
+        entite_id (int): L'ID de l'entité à laquelle le document est associé.
+        file (UploadFile): Le fichier à ajouter.
+    """
+
     entite_id: int
     file: UploadFile
 
 
 class AddFilesToDoc(BaseModel):
+    """
+    Modèle pour l'ajout de plusieurs fichiers à un document.
+
+    Attributes:
+        entite_id (int): L'ID de l'entité à laquelle le document est associé.
+        files (List[UploadFile]): La liste des fichiers à ajouter.
+    """
+
     entite_id: int
     files: List[UploadFile]
--- back/app/schemas/entite_schemas.py
+++ back/app/schemas/entite_schemas.py
@@ -2,6 +2,18 @@ from pydantic import BaseModel, field_validator
 
 
 class EntiteInfo(BaseModel):
+    """
+    Modèle pour les informations d'une entité.
+
+    Attributes:
+        id_e (int): L'ID de l'entité.
+        denomination (str): La dénomination de l'entité.
+        siren (str): Le numéro SIREN de l'entité.
+        type (str): Le type de l'entité.
+        entite_mere (int): L'ID de l'entité mère.
+        centre_de_gestion (str): Le centre de gestion de l'entité.
+    """
+
     id_e: int
     denomination: str
     siren: str
@@ -11,4 +23,13 @@ class EntiteInfo(BaseModel):
 
     @field_validator("id_e", "entite_mere", mode="before")
     def parse_int(cls, v: str):
+        """
+        Valide et convertit les valeurs des champs 'id_e' et 'entite_mere' en entiers.
+
+        Args:
+            v (str): La valeur à convertir.
+
+        Returns:
+            int: La valeur convertie en entier.
+        """
         return int(v)
