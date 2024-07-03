32ae7c8a751c3d9b7186d2f11b525679e3b897a9
BoussairiIkram <98105656+BoussairiIkram@users.noreply.github.com>
2024-07-03 12:25:48 +0000

    Resolution des conflits avec la branche main

--- back/app/services/user_service.py
+++ back/app/services/user_service.py
@@ -255,7 +255,7 @@ def get_user_flux_service(user):
     flux_response = requests.get(
         flux_url,
         auth=get_pastell_auth(user),
-        timeout=settings.request.timeout,
+        timeout=settings.request_timeout,
     )
 
     if flux_response.status_code != 200:
