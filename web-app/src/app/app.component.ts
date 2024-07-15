65672d16242fe4496472045980620ba8069248e9
syji35 <45880189+syji35@users.noreply.github.com>
2024-07-15 09:12:46 +0000

    Modification de l'init de l'app

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { UserService } from './services/user.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FluxService } from './services/flux.service';
 
 
 
