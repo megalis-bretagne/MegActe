b1b999275a57902847d4554f608621cde138f692
syji35 <45880189+syji35@users.noreply.github.com>
2024-09-27 15:13:19 +0000

    ajout du bouton pour transmettre

--- web-app/src/app/shared/pipes/state-document.pipe.ts
+++ web-app/src/app/shared/pipes/state-document.pipe.ts
@@ -27,7 +27,14 @@ export class StateDocumentPipe implements PipeTransform {
     'recu-iparapheur': 'Transmis au parapheur',
     'fatal-error': 'Erreur inconnue',
     'modification': 'En cours de rédaction',
-    'creation': 'En cours de rédaction'
+    'creation': 'En cours de rédaction',
+    'send-tdt': 'Transmis au tdt',
+    'acquiter-tdt': "Acquitté par la préfecture",
+    "preparation-send-ged": "Envoi à la GED en cours",
+    "send-ged": "Envoi à la GED terminé",
+    "ar-recu-sae": 'Accusé réception du SAE reçu',
+    "preparation-send-sae": "Envoi à l'achivage",
+    "generate-sip": "Préparation à l'archivage"
   };
 
 }
