d1fbf2f8da0150b69580c0badc8d01bbf7bed212
syji35 <45880189+syji35@users.noreply.github.com>
2024-10-07 15:43:42 +0000

    Ajout du client s2low + refacto avec le client pastell
    AJout config de s2low dans l'application

--- back/app/clients/pastell/api/__init__.py
+++ back/app/clients/pastell/api/__init__.py
@@ -3,8 +3,8 @@ import requests
 
 from ..models.user_info import UserInfo
 from ..models.config import Config
-from ..models.auth import AuthUser
 from ..handlers import call_handler
+from requests.auth import HTTPBasicAuth
 
 
 __all__ = ("entite_api", "ApiPastell")
@@ -15,47 +15,47 @@ class ApiPastell:
     Client pour Pastell V4
     """
 
-    def __init__(self, conf: Config, auth: AuthUser = None) -> None:
+    def __init__(self, conf: Config, auth: HTTPBasicAuth = None) -> None:
         self._config = conf
         self._timeout_s = conf.timeout
         self._version = "v4"
         self._auth = auth
 
-    def auth(self, auth: AuthUser):
+    def auth(self, auth: HTTPBasicAuth):
         self._auth = auth
 
-    def perform_get(self, url, auth: AuthUser = None, **kwagrs):
+    def perform_get(self, url, auth: HTTPBasicAuth = None, **kwagrs):
         return self._perform_request("GET", url, auth=auth, **kwagrs)
 
-    def perform_patch(self, url, data, auth: AuthUser = None):
+    def perform_patch(self, url, data, auth: HTTPBasicAuth = None):
         return self._perform_request("PATCH", url, data=data, auth=auth)
 
-    def perform_delete(self, url, auth: AuthUser = None):
+    def perform_delete(self, url, auth: HTTPBasicAuth = None):
         return self._perform_request("DELETE", url, auth=auth)
 
-    def perform_post(self, url, data=None, files=None, auth: AuthUser = None):
+    def perform_post(self, url, data=None, files=None, auth: HTTPBasicAuth = None):
         return self._perform_request("POST", url, data=data, files=files, auth=auth)
 
-    def get_user_by_id_u(self, id_u: int, auth: AuthUser = None):
+    def get_user_by_id_u(self, id_u: int, auth: HTTPBasicAuth = None):
         """Retourne les infos d'un utilisateur
 
         Args:
             id_u (int): identifiant de l'utilisateur
-            auth (AuthUser, optional): le contexte utilisateur redéfini
+            auth (HTTPBasicAuth, optional): le contexte utilisateur redéfini
         """
 
         response = self.perform_get(f"utilisateur/{id_u}", auth)
         return TypeAdapter(UserInfo).validate_python(response)
 
     def count_documents_by_id_e(
-        self, id_e: int, type: str = None, auth: AuthUser = None
+        self, id_e: int, type: str = None, auth: HTTPBasicAuth = None
     ):
         """Retourne le nombre de document sur une entite
 
         Args:
             id_e (int): l'id_e
             type (str, optional): possibilité de filtré par le type de flux
-            auth (AuthUser, optional): _description_. Defaults to None.
+            auth (HTTPBasicAuth, optional): _description_. Defaults to None.
         """
         query_param = {"id_e": id_e}
         # Dictionnaire pour renommer les clés
@@ -84,18 +84,17 @@ class ApiPastell:
         data=None,
         query_params=None,
         files=None,
-        auth: AuthUser = None,
+        auth: HTTPBasicAuth = None,
     ):
         """
         Méthode générique pour effectuer des requêtes HTTP.
         """
         full_url = f"{self._config.base_url}/{url}"
-        auth = auth.do_auth() if auth is not None else self._auth.do_auth()
         response = requests.request(
             method=method,
             url=full_url,
             data=data,
-            auth=auth,
+            auth=auth if auth is not None else self._auth,
             files=files,
             timeout=self._timeout_s,
             params=query_params,
---
+++ back/app/clients/s2low/api/adapter/ssl.py
@@ -0,0 +1,24 @@
+import ssl
+
+from requests.adapters import HTTPAdapter
+
+
+class SSLAdapter(HTTPAdapter):
+    """
+    SSL Adapter pour la connexion à s2low
+
+    Args:
+        HTTPAdapter (_type_): _description_
+    """
+
+    def __init__(self, certfile, certificate_key):
+        self.context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        self.context.load_cert_chain(
+            certfile=certfile, keyfile=certificate_key, password="U6(p67#E4@Fbbd"
+        )
+
+        super().__init__()
+
+    def init_poolmanager(self, *args, **kwargs):
+        kwargs["ssl_context"] = self.context
+        return super().init_poolmanager(*args, **kwargs)
