e33b2aa2e4f98feb2b7e37032652b7479212e843
Emmanuel Riou <emmanuel.riou@protonmail.com>
2026-05-28 09:30:38 +0200

    Update docker-compose and docker-compose.nuxt.dev.yml to start updated docker-backend-start with --dev option. Set PYTHONDONTWRITEBYTECODE.

--- back/docker-backend-start.sh
+++ back/docker-backend-start.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-alembic upgrade head && fastapi run app/main.py --port 8080
+usage() {
+  echo "$0 [--dev]"
+  exit 1
+}
+
+if [ $# -gt 1 ]; then
+  echo "$0 takes 0 or 1 argument"
+  usage
+fi
+
+mode=${1:---prod}
+
+if [ $mode = "--dev" ]; then
+  alembic upgrade head && fastapi run app/main.py --reload --port 8080
+else
+  fastapi run app/main.py --port 8080
+fi
