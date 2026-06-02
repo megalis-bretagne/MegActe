88c6fa6835e40a7f1573abf001b4f144f38fdfcb
Mendel08 <kmyann2004@gmail.com>
2026-06-02 15:38:02 +0200

    composant DocumentList

--- nuxt-web-app/app/pages/index.vue
+++ nuxt-web-app/app/pages/index.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-const { user } = useUserContext();
-const entiteId = computed(() => user.value?.pastell?.id_e ?? 1);
-</script>
+<script setup></script>
 
 <template>
-  <div class="p-6">
-    <DocumentList :entite-id="entiteId" />
-  </div>
+  <div>Formulaires ici</div>
 </template>
