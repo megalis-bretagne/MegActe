1d68b8a8af55b04816e122de1b8dfcd0743835b2
Emmanuel Riou <emmanuel.riou@protonmail.com>
2026-06-02 09:48:25 +0200

    Add docs to .gitignore

--- nuxt-web-app/server/routes/auth/[...].ts
+++ nuxt-web-app/server/routes/auth/[...].ts
@@ -11,12 +11,23 @@ export default NuxtAuthHandler({
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, user, account }) {
       if (account) {
+        console.log("objects:");
+        console.log({ token, user, account });
         token.accessToken = account.access_token;
-        console.log("account access token: " + token.accessToken);
+        token.accessTokenExpires = account.expires_at
+        token.refreshToken = account.refresh_token
+        token.refreshTokenExpires = Date.now() + (account.refresh_expires_in as number) * 1000
       }
-      return token;
+
+      // If the access token has not expired we return it
+      if (Date.now() < (token.accessTokenExpires as number)) {
+        return token
+      }
+
+      // refresh to access token if it has expired
+      return refreshAccessToken(token)
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
