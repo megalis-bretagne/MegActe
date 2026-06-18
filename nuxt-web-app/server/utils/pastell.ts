d50880b448f69a282966449c7a33f1672ba74ca4
Emmanuel Riou <emmanuel.riou@protonmail.com>
2026-06-18 17:56:06 +0200

    add a timeout value for getPastellUser

--- nuxt-web-app/server/utils/pastell.ts
+++ nuxt-web-app/server/utils/pastell.ts
@@ -1,13 +1,13 @@
 //Get related pastell user from backend
-export async function getPastellUser(accessToken: string): Promise<Void> {
+export async function getPastellUser(accessToken: string): Promise<any> {
   try {
     const config = useRuntimeConfig();
-    console.log("initPastellUser with token: " + accessToken);
     const pastellUser = await $fetch("/user", {
       baseURL: config.public.apiBaseUrl,
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
+      timeout: 1000,
     });
     return pastellUser;
   } catch (error) {
