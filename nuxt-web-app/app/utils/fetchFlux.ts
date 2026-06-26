4377f446bc48c8e2854a198ebe9cf62bf5f49ae4
Emmanuel Riou <emmanuel.riou@protonmail.com>
2026-06-26 17:28:42 +0200

    Add fetchFluxDetails utils function and return proper types

--- nuxt-web-app/app/utils/fetchFlux.ts
+++ nuxt-web-app/app/utils/fetchFlux.ts
@@ -1,8 +1,8 @@
-export async function fetchUserFlux(token: string): Promise<any> {
+export async function fetchUserFlux(token: string): Promise<Record<string, Flux>> {
   try {
     const config = useRuntimeConfig();
     console.log("Fetch user flux with token: " + token);
-    const flux = await $fetch("/user/flux", {
+    const flux: Record<string, Flux> = await $fetch("/user/flux", {
       baseURL: config.public.apiBaseUrl,
       headers: {
         Authorization: `Bearer ${token}`,
@@ -11,6 +11,23 @@ export async function fetchUserFlux(token: string): Promise<any> {
     });
     return flux;
   } catch (error) {
-    console.log("Failed to fetch related Pastell user data:", error);
+    console.log("Failed to fetch user flux: ", error);
+  }
+}
+
+export async function fetchFluxDetails(fluxType: string, token: string): Promise<Record<string, FluxDetails>> {
+  try {
+    const config = useRuntimeConfig();
+    console.log("Fetch flux details for type:" + fluxType);
+    const fluxDetails: Record<string, FluxDetails> = await $fetch(`/flux/${fluxType}`, {
+      baseURL: config.public.apiBaseUrl,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      timeout: 1000,
+    });
+    return fluxDetails;
+  } catch (error) {
+    console.log("Failed to fetch flux details: ", error);
   }
 }
